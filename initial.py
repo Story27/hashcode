2c85c412de5717d7e843b866bbc27b57a3295162
Story <pathakharshit281@gmail.com>
2022-02-25 01:26:40 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,17 +1,3 @@
-file = open("initial.txt")
-lines = file.readlines()
-print(lines)
-
-
-a = open("input.txt","a+")
-b = a.readlines()
-c =len(b)
-for n in (0,c):
-    b[n] = b[n].split("/n")
-print(b)
-
-
-
 anna = {"C++" : "2"}
 bob = {"HTML": "5","CSS" : "5"}
 maria = {"Python" : "3"}
@@ -21,3 +7,27 @@ WebServer = ("7","10","7","2")
 req2 = {"HTML" : "3","C++" : "2"}
 WebChat = ("10","20","20","2")
 req3 = {"Python" : "3","HTML" : "3"}
+
+a = req1.get("C++")
+b = anna.get("C++")
+c = bob.get("CSS")
+d = bob.get("HTML")
+e = maria.get("Python")
+f = req2.get("C++")
+g = req2.get("HTML")
+h = req3.get("HTML")
+i = req3.get("Python")
+diction = {}
+diction['3'] = 'three projects are planned'
+diction['Logging'] = 'assignments for project Logging'
+if a>b or a==b:
+    diction['anna'] = 'Anna → first role'
+
+diction['WebServer'] = 'assignments for project WebServer'
+if (d>g or d==g) and (b>f or b==f):
+    diction['Bob Anna'] = 'Bob → first role, Anna → second role'
+
+diction['WebChat'] = 'assignments for project WebChat'
+if (d>h or d==h) and (e>i or e==i):
+    diction['Maria Bob'] = 'Maria → first role, Bob → second role'
+print(diction)
