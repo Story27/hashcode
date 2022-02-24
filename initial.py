c4e17540c45a44b0167773816b0b71530d4849ec
Story <pathakharshit281@gmail.com>
2022-02-25 00:39:36 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,3 +1,12 @@
 file = open("initial.txt")
 lines = file.readlines()
 print(lines)
+
+
+a = open("input.txt","r")
+b = a.readlines()
+c =len(b)
+for n in (0,c):
+    n=0
+    b[n] = b[n].split("/n")
+print(b)
